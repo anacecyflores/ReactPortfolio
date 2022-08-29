3e290de68573093ba00654f4f91c81e77e687b80
Cecy Flores <anacecyflores1@gmail.com>
2022-08-29 00:22:10 -0500

    updated header info

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/job-profile-pic.JPG";
+import HeaderSocial from "./HeaderSocials";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="container header_container">
+        <h5>Hi,My name is</h5>
+        <h1>Ana Cecy Flores</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+        <HeaderSocial />
+        <div className="me">
+          <img src={ME} alt="ME" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
