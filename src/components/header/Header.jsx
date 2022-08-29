a718ba6e71ea596f8322cd579f4142abd6d009e8
Cecy Flores <anacecyflores1@gmail.com>
2022-08-29 16:48:17 -0500

    updated pictures

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-import ME from "../../assets/me.jpg";
+import ME from "../../assets/me1.JPG";
 import HeaderSocial from "./HeaderSocials";
 
 const Header = () => {
   return (
     <header>
       <div className="container header_container">
-        <h5>Hi,My name is</h5>
+        {/* <h5>Hi,My name is</h5> */}
         <h1>Ana Cecy Flores</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h2 className="text-light">Fullstack Developer</h2>
         <CTA />
         <HeaderSocial />
         <div className="me">
