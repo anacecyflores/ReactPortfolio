efec23417c5e341bfd6d432fe491476e0b1f4b8f
Cecy Flores <anacecyflores1@gmail.com>
2022-08-29 00:19:00 -0500

    Updated Footer "

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import "./footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        To The Top
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+  
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#contact">Contact Me</a>
+        </li>
+      </ul>
+
+      <div className="footer__copyright">
+        <small>&copy; Ana Cecy Flores. All rights Reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
