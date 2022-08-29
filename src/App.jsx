1d29c532ef9c0efdeed700cc1437ccef9c50c5e2
Cecy Flores <anacecyflores1@gmail.com>
2022-08-29 00:32:16 -0500

    Deleted sections of nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
-import Experience from "./components/experience/Experience";
-import Services from "./components/services/Services";
 import Portfolio from "./components/portfolio/Portfolio";
-// removed testimonials section
-// import Testimonials from './components/testimonials/Testimonials'
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 
@@ -17,7 +13,6 @@ export const App = () => {
       <Nav />
       <About />
       <Portfolio />
-      {/* <Testimonials /> */}
       <Contact />
       <Footer />
     </>
